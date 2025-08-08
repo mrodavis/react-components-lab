f0984a2661c7a7d96558f52362d68c126e0eff8d
mrodavis <orville.davis@scrapit.io>
2025-08-08 12:17:19 -0500

    completed lab, refactored code to create sub-components

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,13 +1,13 @@
 // src/components/WeatherForecast/WeatherForecast.jsx
 import './WeatherForecast.css';
+import WeatherIcon from './WeatherIcon';
+import WeatherData from './WeatherData';
 
 const WeatherForecast = ({ day, img, imgAlt, conditions, time }) => {
   return (
     <div className="weather">
-      <h2>{day}</h2>
-      <img src={img} alt={imgAlt} />
-      <p><span>conditions: </span>{conditions}</p>
-      <p><span>time: </span>{time}</p>
+      <WeatherData day={day} conditions={conditions} time={time} />
+      <WeatherIcon img={img} imgAlt={imgAlt} />
     </div>
   );
 };
